83084bf03b063038cdb97939082c3cfc5f637ced
Danny Guo <dannyguo91@gmail.com>
2018-03-25 23:23:47 -0400

    Assume that tab ids are unique across windows
    According to the docs, this is a safe assumption within a browser
    session.

--- src/background.js
+++ src/background.js
@@ -2,10 +2,6 @@ const fontColorContrast = require('font-color-contrast');
 
 const themes = {};
 
-function setTheme(windowId, theme) {
-    browser.theme.update(windowId, theme);
-}
-
 function changeTheme(message, sender) {
     console.log('message:', message, sender);
 
@@ -23,36 +19,24 @@ function changeTheme(message, sender) {
         const tabId = sender.tab.id;
         const windowId = sender.tab.windowId;
 
-        if (themes[windowId]) {
-            themes[windowId][tabId] = theme;
-        } else {
-            themes[windowId] = {
-                [tabId]: theme
-            };
-        }
+        themes[tabId] = theme;
 
         if (sender.tab.active) {
-            setTheme(windowId, theme);
+            browser.theme.update(windowId, theme);
         }
     }
 }
 
 function handleTabActivated(activeInfo) {
     console.log('active tab:', activeInfo);
-    const tabId = activeInfo.tabId;
-    const windowId = activeInfo.windowId;
-    const theme = themes[windowId] && themes[windowId][tabId] ? themes[windowId][tabId] : null;
+    const theme = themes[activeInfo.tabId] || null;
     if (theme) {
-        setTheme(windowId, theme);
+        browser.theme.update(activeInfo.windowId, theme);
     }
 }
 
 function handleTabRemoved(tabId, removeInfo) {
-    if (removeInfo.isWindowClosing) {
-        delete themes[removeInfo.windowId];
-    } else if (themes[removeInfo.windowId]) {
-        delete themes[removeInfo.windowId][tabId];
-    }
+    delete themes[tabId];
 }
 
 browser.runtime.onMessage.addListener(changeTheme);
