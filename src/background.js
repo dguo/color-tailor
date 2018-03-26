84988ab3333d1b32cfdc47063a20554f0f19b621
Danny Guo <dannyguo91@gmail.com>
2018-03-25 23:08:30 -0400

    Change the theme when the active tab changes

--- src/background.js
+++ src/background.js
@@ -1,23 +1,50 @@
 const fontColorContrast = require('font-color-contrast');
 
-async function changeTheme(message) {
-    console.log('message:', message);
-    if (message && message.themeColor) {
+const themes = {};
+
+function setTheme(windowId, theme) {
+    browser.theme.update(windowId, theme);
+}
+
+function changeTheme(message, sender) {
+    console.log('message:', message, sender);
+
+    if (message && message.themeColor && sender && sender.tab) {
         const accentcolor = message.themeColor.map(color =>
             Math.min(255, Math.round(color))
         );
-        const currentWindow = await browser.windows.getLastFocused();
-        browser.theme.update(currentWindow.id, {
+        const theme = {
             colors: {
                 accentcolor,
                 textcolor: fontColorContrast(accentcolor)
             }
-        });
+        };
+
+        const tabId = sender.tab.id;
+        const windowId = sender.tab.windowId;
+
+        if (themes[windowId]) {
+            themes[windowId][tabId] = theme;
+        } else {
+            themes[windowId] = {
+                [tabId]: theme
+            };
+        }
+
+        if (sender.tab.active) {
+            setTheme(windowId, theme);
+        }
     }
 }
 
 function handleTabActivated(activeInfo) {
     console.log('active tab:', activeInfo);
+    const tabId = activeInfo.tabId;
+    const windowId = activeInfo.windowId;
+    const theme = themes[windowId] && themes[windowId][tabId] ? themes[windowId][tabId] : null;
+    if (theme) {
+        setTheme(windowId, theme);
+    }
 }
 
 browser.runtime.onMessage.addListener(changeTheme);
