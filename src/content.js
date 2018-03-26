a9780cd0d026d71a41f2e1446a0ce7e80375bea5
Danny Guo <dannyguo91@gmail.com>
2018-03-25 21:39:20 -0400

    Check for a custom favicon location

--- src/content.js
+++ src/content.js
@@ -1,16 +1,38 @@
 const Vibrant = require('node-vibrant');
 
-async function getDominantColor() {
+function getThemeColor() {
     const themeColorTag = document.querySelector(`meta[name='theme-color']`);
     if (themeColorTag) {
-        const themeColor =
-            themeColorTag.getAttribute('content') ||
-            themeColorTag.getAttribute('value');
-        browser.runtime.sendMessage({themeColor});
-    } else {
-        const color = await Vibrant.from('/favicon.ico').getPalette();
-        browser.runtime.sendMessage({themeColor: color.Vibrant.getRgb()});
+        return themeColorTag.getAttribute('content') ||
+               themeColorTag.getAttribute('value');
     }
+
+    return null;
+}
+
+async function getFavicon() {
+    let faviconLocation;
+
+    const faviconTag = document.querySelector(`link[rel~='icon']`);
+    if (faviconTag) {
+        faviconLocation = faviconTag.getAttribute('href');
+    }
+
+    return faviconLocation || '/favicon.ico';
+}
+
+async function getDominantColor() {
+    let color = getThemeColor();
+
+    if (!color) {
+        const faviconLocation = await getFavicon();
+        console.log('favicon location:', faviconLocation);
+        const palette = await Vibrant.from(faviconLocation).getPalette();
+        color = palette.Vibrant.getRgb();
+    }
+
+    console.log('color:', color);
+    browser.runtime.sendMessage({themeColor: color});
 }
 
 getDominantColor();
